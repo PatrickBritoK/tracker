9fce52cbe8e4b3d0ab5d79fe2f56b7ab7de5e9ba
Patrick Brito <patrickbritokun530@gmail.com>
2024-11-21 07:42:05 -0300

    Adicionando hooks

---
+++ src/hooks/notificador.ts
@@ -0,0 +1,24 @@
+import { TipoDeNotificacao } from "@/interfaces/INotificacao";
+import { store } from "@/store";
+import { NOTIFICAR } from "@/store/tipo-mutacoes";
+
+type Notificador = {
+  notificar: (tipo: TipoDeNotificacao, titulo: string, texto: string) => void;
+};
+
+export default (): Notificador => {
+  const notificar = (
+    tipo: TipoDeNotificacao,
+    titulo: string,
+    texto: string
+  ): void => {
+    store.commit(NOTIFICAR, {
+      titulo,
+      texto,
+      tipo: TipoDeNotificacao.SUCESSO,
+    });
+  };
+  return {
+    notificar,
+  };
+};
--- src/views/projetos/Formulario.vue
+++ src/views/projetos/Formulario.vue
@@ -1,12 +1,12 @@
 <script lang="ts">
 import { TipoDeNotificacao } from "@/interfaces/INotificacao";
-import { notificacaoMixin } from "@/mixins/notificar";
 import { useStore } from "@/store";
 import {
   ALTERA_PROJETO,
   ADICIONA_PROJETO,
 } from "@/store/tipo-mutacoes";
 import { defineComponent } from "vue";
+import useNotificador from '@/hooks/notificador'
 
 export default defineComponent({
   name: "Projetos",
@@ -15,7 +15,6 @@ export default defineComponent({
       type: String,
     },
   },
-  mixins: [notificacaoMixin],
   mounted() {
     if (this.id) {
       const projeto = this.store.state.projetos.find(
@@ -50,8 +49,10 @@ export default defineComponent({
   },
   setup() {
     const store = useStore();
+    const { notificar } = useNotificador()
     return {
       store,
+      notificar
     };
   },
 });
